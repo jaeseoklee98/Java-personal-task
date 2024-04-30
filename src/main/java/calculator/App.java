2f911291c244a7c6a39ae412ffea8b72ce5881c3
Robbie <robbie331@>
2024-04-30 20:06:49 +0900

    Level 1-3 사칙연산 진행 후 결과값 출력

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -15,7 +15,26 @@ public class App {
         System.out.println("사칙연산 기호를 입력하세요: ");
         char operator = sc.next().charAt(0); // charAt(idx) : charAt 메서드는 매개변수로 char 타입으로 반환 하고자하는 문자열의 위치(index)를 받는다.
 
-        System.out.println("operator: " + operator);
+        int result = 0;
+
+        if (operator == '+') {
+            result = num1 + num2;
+        }else if (operator == '-') {
+            result = num1 - num2;
+        }else if (operator == '*') {
+            result = num1 * num2;
+        }else if (operator == '/') {
+            if (num2 == 0) {
+                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+            }else{
+                result = num1 / num2;
+            }
+        }else {
+            System.out.println("올바르지 않은 사칙연산 기호입니다.");
+        }
+
+
+        System.out.println("결과: " + result);
 
     }
 }
