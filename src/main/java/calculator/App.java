9b204cedb39457cd12da9ee0f5b6be5c2892a590
Robbie <robbie331@>
2024-04-30 19:48:21 +0900

    Level 1-2 사칙연산 기호 입력받기

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -12,6 +12,10 @@ public class App {
         System.out.println("두 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt();
 
-        System.out.println("num1, num2 = " + num1 + " + " + num2);
+        System.out.println("사칙연산 기호를 입력하세요: ");
+        char operator = sc.next().charAt(0); // charAt(idx) : charAt 메서드는 매개변수로 char 타입으로 반환 하고자하는 문자열의 위치(index)를 받는다.
+
+        System.out.println("operator: " + operator);
+
     }
 }
